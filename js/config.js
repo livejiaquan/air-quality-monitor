fc533b2ea2a138f0475bc3edf428b77e5c8f81eb
livejiaquan <livejiaquan010313@gmail.com>
2025-12-02 09:59:21 +0800

    Inject API key directly into config.js during build

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
-import { API_KEY } from './apikey.js';
-
+// API_KEY will be injected during build
 const CONFIG = {
     API_URL: 'https://data.moenv.gov.tw/api/v2/aqx_p_432',
-    API_KEY: API_KEY
+    API_KEY: '__API_KEY_PLACEHOLDER__'
 };
 
 export default CONFIG;
